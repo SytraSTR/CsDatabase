aadc27d886334d8034ff30eff75f4c589409d41d
Sytra <phoenixwshadow@gmail.com>
2023-10-23 01:12:03 +0300

    CsDataBase

---
+++ Restaurant/Menu/Menu/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Menu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.textBox1.Location = new System.Drawing.Point(186, 133);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "Lütfen tür seçiniz";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 23);
+            this.button1.TabIndex = 5;
+            this.button1.TabStop = false;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 6;
+            this.button2.TabStop = false;
+            this.button2.Text = "_";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(524, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 30);
+            this.button3.TabIndex = 7;
+            this.button3.TabStop = false;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(122, 110);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.Text = "İçecek";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButtonAll_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(186, 110);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "Yemek";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButtonAll_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(246, 110);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(88, 17);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.Text = "Kahvaltı Türü";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButtonAll_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(340, 110);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(45, 17);
+            this.radioButton4.TabIndex = 12;
+            this.radioButton4.Text = "Tatlı";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButtonAll_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(391, 110);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(53, 17);
+            this.radioButton5.TabIndex = 13;
+            this.radioButton5.Text = "Çorba";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButtonAll_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 292);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Restaurant/Menu/Menu/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Menu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+        }
+
+        static string ConString =
+            "Data Source=DESKTOP-5J370CT\\SYTRA;Initial " +
+            "Catalog=Restaurant;Integrated Security=True";
+        SqlConnection Baglanti = new SqlConnection(ConString);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string tur = "";
+                string adi = "";
+
+                tur = radioButton1.Checked ? radioButton1.Text :
+                        radioButton2.Checked ? radioButton2.Text :
+                        radioButton3.Checked ? radioButton3.Text :
+                        radioButton4.Checked ? radioButton4.Text :
+                        radioButton5.Checked ? radioButton5.Text : "";
+
+                adi = textBox1.Text;
+
+                if (tur != "" && adi != "")
+                {
+                    string Sorgu = 
+                        "SELECT COUNT(*) FROM " +
+                        "KafeteryaMenu WHERE Adi = @Adi";
+                    SqlCommand Command = new SqlCommand(Sorgu, Baglanti);
+                    Command.Parameters.Add("@adi", 
+                        SqlDbType.VarChar).Value = adi;
+
+                    Baglanti.Open();
+                    int SorguSonucu = (int)Command.ExecuteScalar();
+                    Baglanti.Close();
+
+                    if (SorguSonucu == 0)
+                    {
+                        string Kayit = 
+                            "INSERT INTO " +
+                            "KafeteryaMenu (Tur, Adi) VALUES (@tur, @adi)";
+                        SqlCommand Command1 = new SqlCommand(Kayit, Baglanti);
+
+                        Command1.Parameters.Add("@tur", 
+                            SqlDbType.VarChar).Value = tur;
+                        Command1.Parameters.Add("@adi", 
+                            SqlDbType.VarChar).Value = adi;
+
+                        Baglanti.Open();
+                        Command1.ExecuteNonQuery();
+                        Baglanti.Close();
+
+                        MessageBox.Show("Menü kaydedilmiştir.", "Bilgi");
+                        Sil();
+                    }
+                    else
+                    {
+                        MessageBox.Show( textBox1.Text + " zaten mevcut.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Eksik bilgi sebebiyle kaydedilmemiştir.", "Bilgi");
+                }
+            }
+            catch (Exception Hata)
+            {
+                MessageBox.Show("Bir hatayla karşılaştınız;" + Hata.Message, "Hata",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text == "Yiyecek adı" ||
+                textBox1.Text == "İçecek adı" ? "" : textBox1.Text;
+
+            textBox1.ForeColor = textBox1.Text != "İçecek adı" ||
+                textBox1.Text != "Yiyecek adı" ? Color.Black : Color.DimGray;
+        }
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text == "" ? textBox1.Text =
+                radioButton1.Checked ? "İçecek adı" : "Yiyecek adı" : textBox1.Text;
+
+            textBox1.ForeColor = textBox1.Text == "İçecek adı" ||
+                textBox1.Text == "Yiyecek adı" ? Color.DimGray : Color.Black;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Kapatmak istediğinize emin misiniz?",
+                "Kapatma onayı", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes)
+                { Close(); }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox1.Text = textBox1.Text.First().ToString().ToUpper() +
+                    textBox1.Text.Substring(1).ToLower();
+
+                textBox1.SelectionStart = textBox1.Text.Length;
+            }
+        }
+        private void radioButtonAll_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = radioButton1.Checked || radioButton2.Checked ||
+                radioButton3.Checked || radioButton4.Checked ||
+                radioButton5.Checked ? true : false;
+
+            textBox1.Text = radioButton1.Checked ? 
+                "İçecek adı" : "Yiyecek adı";
+        }
+
+        public void Sil()
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+            radioButton5.Checked = false;
+
+            textBox1.Text = "Lütfen tür seçiniz";
+            textBox1.ForeColor = Color.DimGray;
+            textBox1.Enabled = false;
+
+            ActiveControl = null;
+        }
+    }
+}
